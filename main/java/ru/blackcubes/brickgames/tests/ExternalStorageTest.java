3e89ec00f773edff1e847ae74aace477c0848435
Sergey Gubenko <gubenko@vologda.softlab.ru>
2019-01-16 17:23:44 +0300

    2

---
+++ main/java/ru/blackcubes/brickgames/tests/ExternalStorageTest.java
@@ -0,0 +1,62 @@
+package ru.blackcubes.brickgames.tests;
+
+import android.Manifest;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ExternalStorageTest extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        setContentView(textView);
+
+        String state = Environment.getExternalStorageState();
+        if (!state.equals(Environment.MEDIA_MOUNTED)) {
+            textView.setText("No external storage mounted");
+        } else {
+            int requestCode = 1715;
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, requestCode);
+            File externalDir = Environment.getExternalStorageDirectory();
+            File textFile = new File(externalDir.getAbsolutePath() + File.separator + "text.txt");
+            try {
+                writeTextFile(textFile, "Hello world");
+                String text = readTextFile(textFile);
+                textView.setText(text);
+                if (!textFile.delete()) {
+                    textView.setText("Couldn't remove temporary file");
+                }
+            } catch (IOException ex) {
+                textView.setText("Something went wrong: " + ex.getMessage());
+            }
+        }
+    }
+
+    private String readTextFile(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        StringBuilder text = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            text.append(line);
+            text.append("\n");
+        }
+        reader.close();
+        return text.toString();
+    }
+
+    private void writeTextFile(File file, String text) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(text);
+        writer.close();
+    }
+}
